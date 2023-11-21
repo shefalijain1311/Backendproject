9c12da0f643292b231e46ea75ec73a7270c99528
Shefali Jain <114603553+shefalijain1311@users.noreply.github.com>
2023-11-21 17:18:22 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
